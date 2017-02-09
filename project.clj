69ff79e025b6ad6af66ed37411e0f61c6f1ce7f3
Saul Shanabrook <s.shanabrook@gmail.com>
2017-02-08 23:41:21 -0500

    first go at proto file and clojure spec

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://saulshanabrook.com/ici.recorder"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [proto-repl "0.3.1"]
+                 [org.clojure/test.check "0.9.0"]])
