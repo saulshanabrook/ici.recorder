629a96a4ca3394f3dcb20d2ac1ce569c3403f0e0
Saul Shanabrook <s.shanabrook@gmail.com>
2017-04-21 13:35:05 -0400

    Version 0.2.0 [ci skip]

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject ici.recorder "0.1.0-SNAPSHOT"
+(defproject ici.recorder "0.2.0"
   :description "FIXME: write description"
   :url "http://saulshanabrook.com/ici.recorder"
   :license {:name "Eclipse Public License"
