f4dd89276015b32aa856878dcdb6bd2408224ceb
Saul Shanabrook <s.shanabrook@gmail.com>
2017-04-21 13:30:09 -0400

    get lein ready to publish

--- clojure/project.clj
+++ clojure/project.clj
@@ -14,26 +14,24 @@
                   ; :exclusions [org.slf4j/slf4j-api]]
                  [environ "1.1.0"]
                  [potemkin "0.4.3"]
-                 [proto-repl "0.3.1"]
+                ;  [proto-repl "0.3.1"]
                 ;  [com.taoensso/timbre "4.10.0"]
-                 [org.clojure/test.check "0.9.0"]
-                 [criterium "0.4.4"]
+
+
                 ;  [org.apache.logging.log4j/log4j-api "2.8.2"]
                 ;  [org.apache.logging.log4j/log4j-core "2.8.2"]
                 ;  [org.apache.logging.log4j/log4j-1.2-api "2.8.2"]]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [org.apache.hadoop/hadoop-client "2.3.0"]]
-
+  :profiles {:dev {:dependencies [[criterium "0.4.4"]
+                                  [org.clojure/test.check "0.9.0"]]
+                   :plugins [[lein-shell "0.5.0"]]}}
   :exclusions [org.slf4j/slf4j-log4j12]
   :managed-dependencies [; use the same version of this as parquet-hadoop
                          [org.apache.hadoop/hadoop-common "2.3.0"]
                          ; force upgrade this to deal with
                          ; https://github.com/ptaoussanis/carmine/issues/5
                          [org.xerial.snappy/snappy-java "1.1.4-M3"]]
-  :repositories [["localrepo1"  {:url "file:myrepo"
-                                 ;; provide these so doesn't iteractivly ask for config
-                                 :username :env/localrepo_username
-                                 :password :env/localrepo_password}]]
   :jvm-opts ^:replace ["-Xmx16G"
                        "-XX:+UseConcMarkSweepGC"
                        "-XX:+CMSClassUnloadingEnabled"
@@ -41,13 +39,16 @@
                       ;  "-agentpath:/Users/saul/Downloads/YourKit-Java-Profiler-2017.02.app/Contents/Resources/bin/mac/libyjpagent.jnilib"]
 
   :aot :all
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version" "leiningen.release/bump-version"]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :username :env
+                              :sign-releases false
+                              :password :env}]]
+  :release-tasks [["change" "version" "leiningen.release/bump-version"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["shell" "git" "commit" "-am" "Version ${:version} [ci skip]"]
-                  ["vcs" "tag" "v" "--no-sign"] ; disable signing and add "v" prefix
+                  ["shell" "git" "tag" "${:version}"]
                   ["deploy"]
                   ["change" "version" "leiningen.release/bump-version" "qualifier"]
-                  ["shell" "git" "commit" "-am" "Version ${:version} [ci skip]"]
-                  ["vcs" "push"]]
+                  ["shell" "git" "tag" "${:version}"]
+                  ["shell" "git" "push" "--tags"]]
   :global-vars {*warn-on-reflection* true})
